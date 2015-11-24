2e1cfb1fb54deadb987c781cd727260e77d53caf
Nick <nick.fieschko@yale.edu>
2015-11-24 18:21:27 -0500

    Update registration page with new fields, create basic index page of all users

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
   
   devise_for :users, controllers: { registrations: 'registrations' }
-  resources :users, only: [:show]
+  resources :users, only: [:show, :index]
 
   # You can have the root of your site routed with "root"
   root 'pages#home'
