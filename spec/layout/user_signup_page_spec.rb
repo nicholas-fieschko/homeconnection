ff6fc9a2288865de7c3ba910d7436c4687cf3106
Nicholas Fieschko <nicholas.fieschko@yale.edu>
2016-02-06 19:00:17 -0500

    Refactor sign up page spec to be less tightly coupled to implementation

--- spec/layout/user_signup_page_spec.rb
+++ spec/layout/user_signup_page_spec.rb
@@ -7,36 +7,36 @@ feature "Sign up page" do
     before { visit new_user_registration_path }
 
     it "has an email input field" do
-      expect(page).to have_xpath("//input[@name='user[email]']")
+      expect(page).to have_field "Email"
     end
 
     it "has a name input field" do
-      expect(page).to have_xpath("//input[@name='user[name]']")
+      expect(page).to have_field "Name"
     end
 
     it "has a gender input field" do
-      expect(page).to have_xpath("//input[@name='user[gender]']")
+      expect(page).to have_field "Gender"
     end
 
     it "has a birthday input field" do
-      expect(page).to have_xpath("//input[@name='user[birthday]']")
+      expect(page).to have_field "Birthday"
     end
 
     it "has an about-me input field" do
-      expect(page).to have_xpath("//textarea[@name='user[about]']")
+      expect(page).to have_field "About"
     end
 
     it "has an provider-or-seeker input field" do
-      expect(page).to have_xpath("//input[@name='user[provider]']")
+      expect(page).to have_field "Provider"
     end
 
     it "has password and password confirmation input fields" do
-      expect(page).to have_xpath("//input[@name='user[password]']")
-      expect(page).to have_xpath("//input[@name='user[password_confirmation]']")
+      expect(page).to have_field "Password"
+      expect(page).to have_field "Password confirmation"
     end
 
     it "has a submit button" do
-      expect(page).to have_xpath("//input[@type='submit']")
+      expect(page).to have_button "Sign up"
     end
 
   end
